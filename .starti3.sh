38f1fa6960620532bead29257f971d87e8a0788b
Julius Gonsior <julius@gaenseri.ch>
2018-01-16 17:25:29 +0100

    made start script modular

--- .starti3.sh
+++ .starti3.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-$HOME/.screenlayout/standard.sh&
+$HOME/.startLocalStuff.sh&
 feh --bg-fill $HOME/pictures/wallpaper/wallpaper.jpg&
-#wicd-client --tray&
-firefox-nightly&
 setxkbmap -layout de,de -variant neo,basic -option -option grp:sclk_toggle -option grp_led:scroll&
 xset -b&
 redshift-gtk&
 qtpass&
 nm-applet --sm-disable&
 #(sleep 25 && syncthing-gtk) &
-(sleep 25 && telegram-desktop) &
 (sleep 25 && gajim) &
 cat $HOME/.config/i3/config.local $HOME/.config/i3/config.base > $HOME/.config/i3/config
 exec i3 -V >>  /tmp/i3log-$(date +'%F-%k-%M-%S') 2>&1
