f136fa3dc48fd9b23ae326df5326cbc44e7e9bbb
Julius Gonsior <julius@gaenseri.ch>
2018-01-16 15:59:12 +0100

    Fixed gpg?

--- .starti3.sh
+++ .starti3.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-cat $HOME/.config/i3/config.base \
-    $HOME/.config/i3/config.local > $HOME/.config/i3/config
+$HOME/.screenlayout/standard.sh&
+feh --bg-fill $HOME/pictures/wallpaper/wallpaper.jpg&
+#wicd-client --tray&
+firefox-nightly&
+setxkbmap -layout de,de -variant neo,basic -option -option grp:sclk_toggle -option grp_led:scroll&
+xset -b&
+redshift-gtk&
+qtpass&
+nm-applet --sm-disable&
+#(sleep 25 && syncthing-gtk) &
+(sleep 25 && telegram-desktop) &
+(sleep 25 && gajim) &
+cat $HOME/.config/i3/config.local $HOME/.config/i3/config.base > $HOME/.config/i3/config
 exec i3 -V >>  /tmp/i3log-$(date +'%F-%k-%M-%S') 2>&1
