785d6520c03fc664d18c685dcdbc37988f6d45d1
Julius Gonsior <julius@gaenseri.ch>
2019-08-13 16:23:00 +0200

    polybar white

--- .config/polybar/mpris.sh
+++ .config/polybar/mpris.sh
@@ -2,7 +2,7 @@
 
 # Specifying the icon(s) in the script
 # This allows us to change its appearance conditionally
-icon=""
+icon=" "
 
 player_status=$(playerctl status 2> /dev/null)
 if [[ $? -eq 0 ]]; then
